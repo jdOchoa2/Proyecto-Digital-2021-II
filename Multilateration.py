ba69b7faa264695748b47057d6e2ad5272139fe1
Juan David Ochoa Acosta <beyonder88@MacBook-Air-de-Juan.local>
2022-01-12 15:46:46 -0500

    Get Data

--- Multilateration.py
+++ Multilateration.py
@@ -1,68 +1,91 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import serial
+import sys
+
+def ReadSave():
+    File = open("Data.txt",'w')
+    n=int(sys.argv[1])
+    ser = serial.Serial('/dev/cu.usbmodem14101', 9600)
+    print(n)
+    for ii in range(3*n):
+        data = str(ser.readline())
+        File.write(data[2:(len(data)-5)])
+        File.write('\n')
+    File.close()
+
+def Multi(LR,CL,CR):
+    #Neccesary arrays
+    Center = np.zeros(3)
+    a = np.zeros(3)
+    b2 = np.zeros(3)
+    c = np.zeros(3)
+
+    a[0] = LR
+    a[1] = CL
+    a[2] = CR
+    
+    #Hiperbola variables
+    
+    a[:] *= v/(2e6)
+    Center[1] = -L/2
+    Center[2] = L/2
+
+    c[0] = L
+    c[1] = L/2
+    c[2] = L/2
+
+    for jj in range(3):
+        b2[jj] = c[jj]**2 - a[jj]**2
+
+    #Solution
+    A = 1/a[2]**2
+    B = -(b2[1]/(b2[2]*a[1]**2))
+    C = 1 - (b2[1]/b2[2])
+
+    m2 = A+B
+    m1 = -2*(A*Center[2]+B*Center[1])
+    m0 = A*Center[2]**2+B*Center[1]**2-C
+
+    x = -m1/(2*m2)
+    if a[0]>0:
+        if a[2]>0:
+            x += math.sqrt(m1**2-4*m2*m0)/(2*m2)
+        else:
+            x -= math.sqrt(m1**2-4*m2*m0)/(2*m2)
+    else:
+        if a[1]>0:
+            x += math.sqrt(m1**2-4*m2*m0)/(2*m2)
+        else:
+            x -= math.sqrt(m1**2-4*m2*m0)/(2*m2)
+            
+    y=math.sqrt(b2[1]*(((x-Center[1])/a[1])**2-1))
+    return x*100, y*100;
+
+### MAIN ###
+
+#ReadSave()
 
 #Constants
-c = 343
-L = .15
+v = 343
+L = .18
 
-#Example Initial Conditions
-x0 = .05
-y0 = .10
+#Read data from txt file
+Data = np.loadtxt("Data.txt")
+N = int(len(Data)/3)
 
-print("--------------")
-print(x0)
-print(y0)
+#Save in
+X = np.zeros(N)
+Y = np.zeros(N)
 
-t0L = math.sqrt((x0+L)**2+y0**2)/c
-t0C = math.sqrt((x0)**2+y0**2)/c
-t0R = math.sqrt((x0-L)**2+y0**2)/c
+#Estimates origin point of each sound
+for ii in range(N):
+    X[ii],Y[ii]=Multi(Data[ii*3],Data[ii*3+1],Data[ii*3+2])
+    print(X[ii])
+    print(Y[ii])
 
-a = np.zeros(3)
-a[0] = (t0L - t0R)*1e6
-a[1] = (t0L - t0C)*1e6
-a[2] = (t0C - t0R)*1e6
-
-#Hiperbola variables
-
-a[:] *= c/(2e6)
-
-Center = np.zeros(3)
-Center[1] = -L/2
-Center[2] = L/2
-
-c = np.zeros(3)
-c[0] = L
-c[1] = L/2
-c[2] = L/2
-
-b2 = np.zeros(3)
-for i in range(3):
-    b2[i] = c[i]**2 - a[i]**2
-
-#Solution
-A = 1/a[2]**2
-B = -(b2[1]/(b2[2]*a[1]**2))
-C = 1 - (b2[1]/b2[2])
-
-m2 = A+B
-m1 = -2*(A*Center[2]+B*Center[1])
-m0 = A*Center[2]**2+B*Center[1]**2-C
-
-x = -m1/(2*m2)
-if a[0]>0:
-    if a[2]>0:
-        x += math.sqrt(m1**2-4*m2*m0)/(2*m2)
-    else:
-        x -= math.sqrt(m1**2-4*m2*m0)/(2*m2)
-else:
-    if a[1]>0:
-        x += math.sqrt(m1**2-4*m2*m0)/(2*m2)
-    else:
-        x -= math.sqrt(m1**2-4*m2*m0)/(2*m2)
-
-y=math.sqrt(b2[1]*(((x-Center[1])/a[1])**2-1))
-
-print("--------------")
-print(x)
-print(y)
+plt.scatter(X,Y)
+plt.xlim(-10,10)
+plt.ylim(0,25)
+plt.show()
